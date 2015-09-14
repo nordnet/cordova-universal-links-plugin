ad02e21265fd43d51a1d2f0155646b60168524e4
Nikolay Demyankov <nikolay.demyankov@nordnet.se>
2015-09-14 17:05:04 +0200

    Initial commit for iOS version of the plugin

---
+++ src/ios/CULPlugin.h
@@ -0,0 +1,12 @@
+//
+//  CULPlugin.h
+//
+//  Created by Nikolay Demyankov on 14.09.15.
+//
+
+#import <Foundation/Foundation.h>
+#import <Cordova/CDVPlugin.h>
+
+@interface CULPlugin : CDVPlugin
+
+@end
---
+++ src/ios/CULPlugin.m
@@ -0,0 +1,19 @@
+//
+//  CULPlugin.m
+//  TestUL
+//
+//  Created by Nikolay Demyankov on 14.09.15.
+//
+//
+
+#import "CULPlugin.h"
+
+@implementation CULPlugin
+
+- (void)pluginInitialize {
+    
+}
+
+
+
+@end
