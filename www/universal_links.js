27529eaeff6969f5db4f1d699d65037d4a9da8ac
Nikolay Demyankov <nikolay.demyankov@nordnet.se>
2015-12-17 12:05:45 +0100

    Minor cleanup in js module.

--- www/universal_links.js
+++ www/universal_links.js
@@ -4,6 +4,7 @@ var exec = require('cordova/exec'),
   // Reference name for the plugin
   PLUGIN_NAME = 'UniversalLinks',
 
+  // Default event name that is used by the plugin
   DEFAULT_EVENT_NAME = 'didLaunchAppFromLink';
 
 // Plugin methods on the native side that can be called from JavaScript
@@ -14,6 +15,13 @@ pluginNativeMethod = {
 
 var universalLinks = {
 
+  /**
+   * Subscribe to event.
+   * If plugin already captured that event - callback will be called immidietly.
+   *
+   * @param {String} eventName - name of the event you are subscribing on; if null - default plugin event is used
+   * @param {Function} callback - callback that is called when event is captured
+   */
   subscribe: function(eventName, callback) {
     if (!callback) {
       console.warn('Universal Links: can\'t subscribe to event without a callback');
@@ -21,19 +29,24 @@ var universalLinks = {
     }
 
     if (!eventName) {
-      eventName = 'didLaunchAppFromLink';
+      eventName = DEFAULT_EVENT_NAME;
     }
 
     var innerCallback = function(msg) {
-      callback(msg.event, msg.data);
+      callback(msg.data);
     };
 
     exec(innerCallback, null, PLUGIN_NAME, pluginNativeMethod.SUBSCRIBE, [eventName]);
   },
 
+  /**
+   * Unsubscribe from the event.
+   *
+   * @param {String} eventName - from what event we are unsubscribing
+   */
   unsubscribe: function(eventName) {
     if (!eventName) {
-      eventName = 'didLaunchAppFromLink';
+      eventName = DEFAULT_EVENT_NAME;
     }
 
     exec(null, null, PLUGIN_NAME, pluginNativeMethod.UNSUBSCRIBE, [eventName]);
