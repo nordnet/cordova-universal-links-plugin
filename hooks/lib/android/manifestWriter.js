c85df9d91a2f26c6eeef49c340976bb4dc0a41e8
Bruno Souza <bruno.souza@ecs.com.br>
2018-04-10 13:07:48 -0300

    Update manifestWriter.js to fix #133 in Cordova 8 with backwards compatible

--- hooks/lib/android/manifestWriter.js
+++ hooks/lib/android/manifestWriter.js
@@ -19,6 +19,11 @@ module.exports = {
  */
 function writePreferences(cordovaContext, pluginPreferences) {
   var pathToManifest = path.join(cordovaContext.opts.projectRoot, 'platforms', 'android', 'AndroidManifest.xml');
+
+  if (!fs.existsSync(pathToManifest)) {
+    pathToManifest = path.join(cordovaContext.opts.projectRoot, 'platforms', 'android', 'app', 'src', 'main', 'AndroidManifest.xml');
+  }
+  
   var manifestSource = xmlHelper.readXmlAsJson(pathToManifest);
   var cleanManifest;
   var updatedManifest;
